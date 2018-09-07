b81247b617e61a670231dbe3d8a18def885b299f
nickgreenquist <nickgreenquist@gmail.com>
2018-09-07 17:31:01 -0400

    add program that always puts 'A' into a buffer

---
+++ reader.go
@@ -0,0 +1,14 @@
+package main
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+func (reader MyReader) Read(b []byte) (int, error) {
+  b[0] = 'A'
+  return 1, nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
